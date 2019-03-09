dd73f3a1067ba6c9e49c4160991bc7da096b6692
AndrzejSwiecicki <aj.swiecicki@gmail.com>
2019-03-09 13:09:00 +0100

    zadanieGotowe

--- src/Warsztat_zadanie_1/Zadanie1.java
+++ src/Warsztat_zadanie_1/Zadanie1.java
@@ -1,7 +1,35 @@
 package Warsztat_zadanie_1;
 
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Zadanie1 {
     public static void main(String[] args) {
+
+
+        //losowanie liczby
+        Random rnd = new Random();
+        int r = rnd.nextInt(3);
+
+        int liczba =0;
+        do {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Zgadnij liczbę: ");
+                while(!scan.hasNextInt()){
+                System.out.println("to nie jest liczba");
+                scan.nextLine();
+                }
+            liczba = scan.nextInt();
+            if (liczba>r){
+                System.out.println("Podałeś za dużą liczbę");
+            } else if(liczba<r){
+                System.out.println("Podałeś za małą liczbę");
+            }else {
+                System.out.println("zgadłeś");
+            }
+
+        }while (liczba != r);
 
     }
 }
