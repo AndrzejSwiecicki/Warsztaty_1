297c26ea30b3e292d0d7b592e5550be1235d59bc
AndrzejSwiecicki <aj.swiecicki@gmail.com>
2019-03-27 19:52:19 +0100

    zadanie3

--- src/Warsztat_zadanie_3/Zadanie3.java
+++ src/Warsztat_zadanie_3/Zadanie3.java
@@ -5,31 +5,34 @@ import java.util.Scanner;
 public class Zadanie3 {
     public static void main(String[] args) {
 
-        System.out.println("Pomyśl liczbę od 0 do 1000");
+        String line = "";
         int min = 0;
         int max = 1000;
-        String zaDuzo = "za dużo";
-        String zaMalo = "za mało";
-        String zgadl = "zgadłeś";
+        int guess = 1000;
 
+        System.out.println("Wymyśl liczbę od 0 do 1000, a ja ją zgadnę max. w 10 próbach !");
 
-        for (int i =0; i<=10;i++){
-            int guess = (((max-min)/2) + min);
-            System.out.println("Zgaduję" + guess);
-            Scanner scr = new Scanner(System.in);
-            String line = scr.next();
-                for(int j = 0;j<10;j++){
-                }
-                if(line.equals(zaDuzo)){
-                    max = guess;
-                } else if (line.equals(zaMalo)){
-                    min = guess;
-
-                }else if (line.equals(zgadl)){
-                    System.out.println("Wygrałem!");
-                    break;
-                }
-
-        }
+            while(!line.equals("zgadłeś")) {
+                guess = (((max - min) / 2) + min);
+                System.out.println("Zgaduję liczbę" + " " + guess);
+                System.out.println("Pobierz odpowiedź z zestawu: -zaDuzo, -zaMalo, -zgadłeś ");
+                Scanner scan = new Scanner(System.in);
+                line = scan.next();
+                    for (int j = 0; j < 10; j++) {
+                        if (line.equals("zgadłeś")) {
+                            System.out.println("Wygrałem!");
+                            break;
+                        } else {
+                            if (line.equals("zaDuzo")) {
+                                max = guess;
+                            } else if (line.equals("zaMalo")) {
+                                min = guess;
+                            } else {
+                                System.out.println("Nie ouszukuj");
+                            }
+                         }
+                    }
+                 }
     }
 }
+
